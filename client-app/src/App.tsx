bf39fd60e2279f27d841f84d0fc7006da1860428
agailloty <mr.clerisaxel@hotmail.fr>
2022-02-10 00:06:48 +0100

    Fin section 3

---
+++ client-app/src/App.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import { Header, List } from 'semantic-ui-react';
+import './App.css';
+import axios from 'axios';
+
+
+function App() {
+  const [activities, setActivities] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:5000/activities").then(response => {
+      setActivities(response.data);
+    })
+  }, [])
+  console.log(activities);
+  return (
+    <div>
+      <Header as='h2' icon='users' content='Reactivities'/>
+        <List>
+          {activities.map((activity: any) => (
+            <List.Item key={activity.id}>
+              {activity.title}
+            </List.Item>
+          ))}
+       </List>
+    </div>
+  );
+}
+
+export default App;
